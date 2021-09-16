3ca7c74fc2fca2ef7fd6e292cdc37365c45ffbbe
Shannon Lucas <shannonlucas4@gmail.com>
2021-09-16 12:37:01 -0700

    add testing

--- filesystem/walk/actions_test.go
+++ filesystem/walk/actions_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -31,6 +35,79 @@ func TestFilterOut(t *testing.T) {
 
 			if f != tc.expected {
 				t.Errorf("got '%t' but expected '%t'\n", f, tc.expected)
+			}
+		})
+	}
+}
+
+func TestRunArchive(t *testing.T) {
+	testCases := []struct {
+		name         string
+		cfg          config
+		extNoArchive string
+		numArchive   int
+		numNoArchive int
+	}{
+		{
+			name: "ArchiveExtensionNoMatch",
+			cfg: config{
+				ext: ".log",
+			},
+			extNoArchive: ".gz",
+			numArchive:   0,
+			numNoArchive: 10,
+		},
+		{
+			name: "ArchiveExtensionMatch",
+			cfg: config{
+				ext: ".log",
+			},
+			extNoArchive: "",
+			numArchive:   10,
+			numNoArchive: 0,
+		},
+		{
+			name: "ArchiveExtensionMixed",
+			cfg: config{
+				ext: ".log",
+			},
+			extNoArchive: ".gz",
+			numArchive:   5,
+			numNoArchive: 5,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var buffer bytes.Buffer
+
+			// create both the origin dir and the archiving dir
+			tempDir, cleanup := createTempDir(t, map[string]int{
+				tc.cfg.ext:      tc.numArchive,
+				tc.extNoArchive: tc.numNoArchive,
+			})
+			defer cleanup()
+
+			archiveDir, cleanupArchive := createTempDir(t, nil)
+			defer cleanupArchive()
+
+			tc.cfg.archive = archiveDir
+
+			if err := run(tempDir, &buffer, tc.cfg); err != nil {
+				t.Fatal(err)
+			}
+
+			pattern := filepath.Join(tempDir, fmt.Sprintf("*%s", tc.cfg.ext))
+			expFiles, err := filepath.Glob(pattern)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			expOut := strings.Join(expFiles, "\n")
+			got := strings.TrimSpace(buffer.String())
+
+			if got != expOut {
+				t.Errorf("got %q but want %q", got, expOut)
 			}
 		})
 	}
